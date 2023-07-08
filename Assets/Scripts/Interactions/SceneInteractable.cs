d4f118a62a8de7697dcfc1599600ac74f097996b
Anthony Doucet <anthonydoucet5417@hotmail.com>
2023-07-07 22:35:40 -0700

    scene transitions

--- Assets/Scripts/Interactions/SceneInteractable.cs
+++ Assets/Scripts/Interactions/SceneInteractable.cs
@@ -8,11 +8,11 @@ namespace Interactions
     public class SceneInteractable : Interactable
     {
         public string sceneName;
-
+        public Animator transition;
+        public float transitionTime = 1f;
         protected override void RunInteraction()
         {
-            Debug.Log("Play animation here!");
-            SceneManager.LoadScene(sceneName);
+            StartCoroutine(LevelTransition(sceneName));
         }
 
         public override void MarkAsCurrentInteractable()
@@ -21,6 +21,14 @@ namespace Interactions
 
         public override void UnMarkAsCurrentInteractable()
         {
+        }
+
+        IEnumerator LevelTransition(string sceneName)
+        {
+            transition.SetTrigger("Start");
+            yield return new WaitForSeconds(transitionTime);
+            SceneManager.LoadScene(sceneName);
+
         }
     }
 }
