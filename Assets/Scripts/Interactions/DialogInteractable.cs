87b7432c1c117470528fd6d1929e054a5ebfa6e6
Trevor LeBlanc <billbobphil@hotmail.com>
2023-07-08 08:55:54 -0700

    Added interaction notice. Added small vertical float/bounce effect. Added new subclass of interactable that expects to produce a notice.

---
+++ Assets/Scripts/Interactions/DialogInteractable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Interactions
+{
+    public class DialogInteractable : InteractableWithNotice
+    {
+        protected override void RunInteraction()
+        {
+            Debug.Log("Here is my cool text!");
+        }
+    }
+}
