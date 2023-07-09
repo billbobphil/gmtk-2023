7841e758b3638bd57b9c91c8792a880c245cec72
Trevor LeBlanc <billbobphil@hotmail.com>
2023-07-08 17:53:27 -0700

    Added a bunch of character art and support in the conversation manager

--- Assets/Scripts/Dialog/Conversation.cs
+++ Assets/Scripts/Dialog/Conversation.cs
@@ -9,10 +9,28 @@ namespace Dialog
     public class Conversation : MonoBehaviour
     {
         [SerializeField] private List<Dialog> conversationSequence;
+        private string _playerName = "Takeout-Terry";
+        [SerializeField] private string OtherName;
+        [SerializeField] private Sprite otherPortrait;
 
         public List<Dialog> GetConversationSequence()
         {
             return conversationSequence;
+        }
+
+        public string GetPlayerName()
+        {
+            return _playerName;
+        }
+
+        public string GetOtherName()
+        {
+            return OtherName;
+        }
+
+        public Sprite GetOtherPortrait()
+        {
+            return otherPortrait;
         }
     }
 }
